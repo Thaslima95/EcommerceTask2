e4fc2b52ef9ca5d748bb97981818ba242230d48e
Thaslima <nizuthasli15@gmail.com>
2023-09-21 05:28:08 +0530

    change

--- frontend/src/Components/Dropdown/Dropdownbox.jsx
+++ frontend/src/Components/Dropdown/Dropdownbox.jsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 import ApiCalls from "../../API/ApiCalls";
 import { Form } from "react-bootstrap";
-import { useState, useEffect } from "react";
+import { useState, useMemo } from "react";
 
 import { useNavigate } from "react-router-dom";
 
@@ -12,15 +12,14 @@ export default function Dropdownbox() {
   const [categories, setCategories] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState("");
 
-  useEffect(() => {
-    getCategoriesDetails();
-  }, []);
-
   const getCategoriesDetails = () => {
     ApiCalls.getCategoris()
       .then((res) => setCategories(res))
       .catch((err) => console.log(err));
   };
+  useMemo(() => {
+    getCategoriesDetails();
+  }, []);
   const handleCategoryChange = (e) => {
     const selectedValue = e.target.value;
     setSelectedCategory(selectedValue);
